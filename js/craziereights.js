46affc27cc858e8d7410315cce75b282071085d5
Blake Leonard <bleonard252@rocketmail.com>
2020-01-25 14:39:33 -0500

    add some warnings, etc

--- js/craziereights.js
+++ js/craziereights.js
@@ -1,5 +1,7 @@
+/** The Crazier Eights root function object. */
 var cz8 = {};
 
+/** The decks within the game. The dealer will use these to shuffle and deal cards. */
 cz8.decks = {};
 cz8.decks.uno = [
     "R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R+2", "RR", "RS",
@@ -14,6 +16,10 @@ cz8.decks.uno = [
     "W", "W+4",
 ];
 Object.defineProperty(cz8.decks.uno, "displayname", {writable: false, value: "Standard deck"})
+/**
+ * Parses a card for display.
+ * @param {string} card The card code to interpret.
+ */
 cz8.parseCard = function parseCard(card) {
     let color = "wild";
     if (card.substr(0,1) == "R") color = "red";
@@ -29,3 +35,37 @@ cz8.parseCard = function parseCard(card) {
     if (card.substr(1) == "S") action = "skip";
     return {color, value, action}
 };
+
+cz8.host = {};
+/** Called when the dealer presses the Continue button. */
+cz8.host.start_playerlist = function start_playerlist() {
+    $()
+};
+
+/** Changes the page.
+ * @param {"host-start"} to The page to switch to.
+ */
+cz8.page = function page(to) {
+    $("main:not(.d-none)").addClass('d-none');
+    $("main[type='"+to+"']").removeClass('d-none');
+}
+
+$(document).ready(() => {
+    if (!window.URLSearchParams) { $("[type='host-start'] .mt-3").append(`<div class="alert alert-danger" style="margin-top: 72px;" role="alert">ERROR: You're not running a new enough browser. Crazier Eights is developed against modern browsers. Older versions of these browsers, and Internet Explorer, are not supported. To make this message disappear, and to make links work, you'll need to use a browser like Firefox. You will have to use the Join button and trim your link to join a game.</div>`); 
+        var url = {} }
+    else var url = new URLSearchParams(location.search);
+    window.cz8local = {url};
+    if (url['room']) {
+        window.cz8local.room = url['room']
+        window.cz8local.homeserver = window.cz8local.room.replace(/^.*\:/,""); //get the homeserver, needed to initialize the SDK
+        window.cz8local.client = matrixcs.createClient(window.cz8local.homeserver || "https://matrix.org"); //this will be re-initialized later (if you're the dealer), maybe?
+        window.cz8local.client.registerGuest()
+            .then((user) => {
+                window.cz8local.user = user;
+                console.log(user);
+            })
+        //TODO: switch pages
+    } else {
+        cz8.page("host-start");
+    }
+})
